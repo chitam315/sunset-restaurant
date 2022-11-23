3816729fda228b137405fbffbcdee9f854171027
chitam315 <ITITIU19048@student.hcmiu.edu.vn>
2022-11-23 23:25:48 +0700

    test deploy 5

--- app.js
+++ app.js
@@ -1,16 +1,55 @@
-const http = require('http');
-const port = process.env.PORT || 3000
+const express = require('express');
+const path = require('path');
 
-var fs = require('fs');
+const app = express();
+const port = process.env.PORT || 3000;
 
-const server = http.createServer(function (req, res) {
-  fs.readFile('index.html', function(err, data) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    return res.end();
-  });
-})
+app.use(express.static(__dirname));
 
-server.listen(port,() => {
-  console.log(`Server running at port `+port);
+// sendFile will go here
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '/index.html'));
 });
+
+app.get('/aboutus', function(req, res) {
+    res.sendFile(path.join(__dirname, '/aboutus.html'));
+  });
+
+  app.get('/contact', function(req, res) {
+    res.sendFile(path.join(__dirname, '/contact.html'));
+  });
+
+  app.get('/event-detail', function(req, res) {
+    res.sendFile(path.join(__dirname, '/event-detail.html'));
+  });
+
+  app.get('/event', function(req, res) {
+    res.sendFile(path.join(__dirname, '/event.html'));
+  });
+
+  app.get('/library', function(req, res) {
+    res.sendFile(path.join(__dirname, '/library.html'));
+  });
+
+  app.get('/menu', function(req, res) {
+    res.sendFile(path.join(__dirname, '/menu.html'));
+  });
+
+  app.get('/place-detail', function(req, res) {
+    res.sendFile(path.join(__dirname, '/place-detail.html'));
+  });
+
+  app.get('/place', function(req, res) {
+    res.sendFile(path.join(__dirname, '/place.html'));
+  });
+
+  app.get('/service-detail', function(req, res) {
+    res.sendFile(path.join(__dirname, '/service-detail.html'));
+  });
+
+  app.get('/service', function(req, res) {
+    res.sendFile(path.join(__dirname, '/service.html'));
+  });
+
+app.listen(port);
+console.log('Server started at http://localhost:' + port);
